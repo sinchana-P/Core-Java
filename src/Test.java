0c407a6cbc7c773fe27ec816ce3a489f0949a3ee
Sinchana P <sinchana.p.swarna@gmail.com>
2023-11-05 09:47:48 +0530

    first java program

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,19 @@
 public class Test {
-    public static void main(String[] args) {
-        System.out.println("This is a Test Class");
+
+    public static void main(String args[]){         // C-Style Array declaration
+        System.out.println("Hello World!!");
+        System.out.print("Hello World");
+    }
+
+}
+
+class Student {
+    public static void main(String[] args){         // Java-Style Array declaration
+        System.out.println("This is a Student Class");
     }
 }
+
+
+// 1. Keyword
+// 2. Identifier
+// 3. Main Function - JVM identifies only this function as Starting point
